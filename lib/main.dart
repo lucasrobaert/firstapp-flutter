aeb413259c8f85f31c6299ec3c4f0b4678ed4f5d
Lucas Roabert <lucasrobaert@hotmail.com>
2021-05-23 20:23:27 -0300

    Navegar para uma nova tela

--- lib/main.dart
+++ lib/main.dart
@@ -66,11 +66,44 @@ class _RandowWordsState extends State<RandowWords> {
     );
   }
 
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute(
+          builder: (BuildContext context){
+            final tiles = _saved.map(
+                (WordPair pair){
+                  return ListTile(
+                    title: Text(
+                      pair.asPascalCase,
+                      style: _biggerFont,
+                    ),
+                  );
+                }
+            );
+            final divided = ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+            ).toList();
+
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Saved Suggestions'),
+              ),
+              body: ListView(children: divided,),
+            );
+          }
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved,)
+        ],
       ),
       body: _buildSuggestions(),
     );
